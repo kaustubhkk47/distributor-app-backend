cb783529638266981c94c5cb580c0fdc292baa9b
Ankit Agarwal <ankitagarwal2204@gmail.com>
2016-03-08 21:52:06 +0000

    settings.py edited online with Bitbucket

--- settings.py
+++ settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'DistributorApp',
+        'NAME': os.path.join(BASE_DIR, 'temp/db.sqlite3'),
     }
 }
 
