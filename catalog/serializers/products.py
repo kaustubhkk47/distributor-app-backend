bd191ab60d8005bf2917a65b2e8a0be187b7553c
Aditya Jha <batu.aditya007@gmail.com>
2016-04-01 01:59:38 +0530

    get products

---
+++ catalog/serializers/products.py
@@ -0,0 +1,23 @@
+
+import time
+
+def serialize_product(productItem):
+    product = {
+        "productID": productItem.id,
+        "distributorID": productItem.distributor_id,
+        "name": productItem.name,
+        "unit": productItem.unit,
+        "price_per_unit": productItem.price_per_unit,
+        "created_at": time.mktime(productItem.created_at.timetuple()),
+        "updated_at": time.mktime(productItem.updated_at.timetuple())
+    }
+    return product
+
+
+def products_parser(productQuerySet):
+    products = []
+
+    for i in range(len(productQuerySet)):
+        products.append(serialize_product(productQuerySet[i]))
+
+    return products
