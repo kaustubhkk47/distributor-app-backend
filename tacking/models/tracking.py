16232752234c204dd03849b9114286861844c70c
Aditya Jha <batu.aditya007@gmail.com>
2016-04-03 06:15:29 +0530

    started new app tracking

---
+++ tacking/models/tracking.py
@@ -0,0 +1,15 @@
+
+from django.db import Models
+
+from users.models.salesman import Salesman
+
+class Tacking(models.Model):
+
+    salesman = models.ForeignKey(Salesman)
+    latlngs = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.salesman.name
