062f4df679a899b1d98f5dbb9a7308788422ad97
Aditya Jha <batu.aditya007@gmail.com>
2016-04-05 13:20:16 +0530

    changes

--- catalog/serializers/offers.py
+++ catalog/serializers/offers.py
@@ -8,7 +8,7 @@ def serialize_offer(offerItem):
         "offer_status": offerItem.offer_status,
         #"created_at": time.mktime(offerItem.created_at.timetuple()),
         "created_at": offerItem.created_at,
-        "updated_at": time.mktime(offerItem.updated_at.timetuple())
+        "updated_at": time.mktime(offerItem.updated_at.timetuple())*1000
         #"updated_at": offerItem.updated_at
     }
     return offer
