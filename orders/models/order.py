771cc9ffc16cf2c8ae061b684f80b014bc476e84
Aditya Jha <batu.aditya007@gmail.com>
2016-04-08 15:47:21 +0530

    order api changes

--- orders/models/order.py
+++ orders/models/order.py
@@ -15,5 +15,10 @@ class Order(models.Model):
     editedPrice = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     productCount = models.IntegerField(default=0)
 
+    orderOffer = models.CommaSeparatedIntegerField(max_length=100, blank=True, null=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.distributor.company_name
